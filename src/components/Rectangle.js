bd925f9dfe71efd768dd77ae48d10a31677844ff
CodeSandbox <support@codesandbox.io>
2023-11-04 12:53:15 +0000

    Sandbox snapshot commit

--- src/components/Rectangle.js
+++ src/components/Rectangle.js
@@ -6,8 +6,6 @@ import Container from 'react-bootstrap/Container';
 
 export const Rectangle_1 = () => {
   return (
-    <Container className="position-absolute top-0 start-100 translate-middle">
     <div className="Rectangle_1" style={{ backgroundImage: `url(${reactRectangle})` }}></div>
-    </Container>
   );
 };
