6058c464c83e45e297e94a8c3d09123cf45f96b0
Shriya285 <shriyakonduru@gmail.com>
2023-11-06 18:51:50 +0530

    Rectangle added

--- src/components/Rectangle.js
+++ src/components/Rectangle.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './Rectangle.css';
 import reactRectangle from '../assets/img/Rectangle.svg';
-import Container from 'react-bootstrap/Container';
-
 
 export const Rectangle_1 = () => {
   return (
-    <div className="Rectangle_1" style={{ backgroundImage: `url(${reactRectangle})` }}></div>
+    <div className="Rectangle_1" style={{ backgroundImage: `url(${reactRectangle})`, width: '100vw', position: 'absolute', top: '0' }}>
+    </div>
   );
 };
