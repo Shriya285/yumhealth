d14fa5f0ab3889930a835697cb863cf333a5ca8f
Shriya285 <shriyakonduru@gmail.com>
2023-11-23 08:45:12 +0530

    header

--- src/App.js
+++ src/App.js
@@ -6,12 +6,11 @@ import { Rectangle_1 } from "./components/Rectangle";
 import { Banner_1 } from "./components/Banner";
 import { Home } from "./components/Home";
 import Login_1 from "./components/Login";
-import  Recipes  from './components/Recipes';
-import  Recipe  from './components/Recipe';
-import  About  from './components/About';
-import  Footer  from './components/Footer';
-
-
+import Recipes from './components/Recipes';
+import Recipe from './components/Recipe';
+import About from './components/About';
+import Footer from './components/Footer';
+import Header from './components/Header'; // Removed the duplicate import
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -31,10 +30,11 @@ function App() {
           <Route path="/login" element={<Login_1 />} />
           <Route path="/other" element={<Rectangle_1 />} />
           <Route path="/recipes" element={<Recipes />} />
+          <Route path="/faqs" element={<Header />} />
           <Route path="/recipe" element={<Recipe />} />
           <Route path="/About-us" element={<About />} />
         </Routes>
-      <Footer />
+        <Footer />
       </div>
     </Router>
   );
